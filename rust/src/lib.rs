f91b0d8f544e91ff6395be392b7e1c0595042d10
WalquerX <wx.vr@outlook.com>
2026-09-17 03:01:12 -0500

    push0 to push7

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,14 +9,43 @@ pub struct EvmResult {
 }
 
 pub fn evm(code: &[u8]) -> EvmResult {
-    let stack: Vec<U256> = Vec::new();
+    let mut stack: Vec<U256> = Vec::new();
     let mut pc = 0;
 
     while pc < code.len() {
-        let _opcode = code[pc];
+        let opcode = code[pc];
         pc += 1;
 
         // TODO: implement the EVM here!
+
+        if opcode == 0x5f /* PUSH0 */ {
+            stack.push(U256::zero());
+        } else if opcode == 0x60 /* PUSH1 1 */ {
+
+            stack.push(U256::from_big_endian(&code[pc..pc + 1]));
+            pc += 1;
+        
+        } else if opcode == 0x61 /* PUSH2 */ {
+            stack.push(U256::from_big_endian(&code[pc..pc + 2]));
+            pc += 2;
+        } else if opcode == 0x62 /* PUSH3 */ {
+            stack.push(U256::from_big_endian(&code[pc..pc + 3]));
+            pc += 3;
+        } else if opcode == 0x63 /* PUSH4 */ {
+            stack.push(U256::from_big_endian(&code[pc..pc + 4]));
+            pc += 4;
+        } else if opcode == 0x64 /* PUSH5 */ {
+            stack.push(U256::from_big_endian(&code[pc..pc + 5]));
+            pc += 5;
+        } else if opcode == 0x65 /* PUSH6 */ {
+            stack.push(U256::from_big_endian(&code[pc..pc + 6]));
+            pc += 6;
+        } else if opcode == 0x66 /* PUSH7 */ {
+            stack.push(U256::from_big_endian(&code[pc..pc + 7]));
+            pc += 7;
+        } else {}
+
+
     }
 
     EvmResult {
